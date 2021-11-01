a3b01e6bc29aaa7f61b13288aadf46c76e9e99b4
Yvan <contact@yvan.dev>
2021-11-01 18:56:15 +0100

    Activation du rappel

--- cogs/config.py
+++ cogs/config.py
@@ -19,9 +19,21 @@ class Config(commands.Cog):
         if not channel:
             channel = ctx.channel
 
+        config["shower"]["enabled"] = True
         config["shower"]["reminder_channel"] = channel.id
         await self.bot.db.update_config(ctx.guild.id, config)
         await ctx.send(f"Les rappels seront désormais envoyés dans le salon: {channel.mention}")
+
+    @commands.command()
+    @commands.has_guild_permissions(manage_guild=True)
+    async def disable_reminders(self, ctx):
+        config = await self.bot.db.get_config(ctx.guild.id)
+
+        config["shower"]["enabled"] = False
+
+        await self.bot.db.update_config(ctx.guild.id, config)
+
+        await ctx.send(f"Les rappels sont désormais désactiver, pour les réactiver utilisez la commande `{ctx.prefix}set_reminder_channel")
 
     @commands.command()
     async def config(self, ctx):
